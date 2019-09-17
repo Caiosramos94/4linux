48dff87d1ca6dc1558271ab96562293cf40b122c
caiosramos94@gmail.com <caiosramos94@gmail.com>
2019-09-16 22:18:06 -0300

    primeiro dia

---
+++ ex01.py
@@ -0,0 +1,77 @@
+nota1 = float(input("Qual e a sua nota 1?: "))
+nota2 = float(input("qual e a sua nota 2?: "))
+nota3 = float(input("qual e a sua nota 3?: "))
+media = (nota1 + nota2 + nota3)/3
+if media >7:
+    print ("aprovado")
+if media <4:
+    print("reprovado")
+else:
+    print("recuperação")
+
+
+exit()
+nota1 = float(input("Qual e a sua nota 1?: "))
+nota2 = float(input("qual e a sua nota 2?: "))
+nota3 = float(input("qual e a sua nota 3?: "))
+media = (nota1 + nota2 + nota3)/3
+if media >7:
+    print ("aprovado")
+    if media <4:
+        print("reprovado")
+    else:
+        print("recuperação")
+    
+
+        exit()
+idade = int(input( "Quantos anos voce tem? :"))
+if idade < 18:
+    print ("voce nao pode dirigir")
+else:    
+    hab = str( input("voce tem habilitacao? :"))
+if idade >= 18 and hab == 'sim':
+   print("você pode dirigir")
+else:
+    print("nao pode dirigir")
+    
+        
+exit()
+idade = int(input( "Quantos anos voce tem? :"))
+if idade >= 18:
+    habilitacao = str( input("voce tem habilitacao? :"))
+    if habilitacao == "sim":
+        print ("pode dirigir")
+    else:
+        print("nao pode dirigir")
+else:
+    print("nao pode dirigir")
+                
+        
+
+
+
+
+
+
+exit()
+
+num1= int(input("Digite o primeiro numero"))
+num2= int(input("Digite o segundo numero"))
+if num1 > num2: 
+    print('num1 e maior que n2')
+else:   
+    print('n2 e maior')
+
+        
+
+
+exit ()
+
+num1= int(input("Digite o primeiro numero"))
+num2= int(input("Digite o segundo numero"))
+soma = num1 + num2
+print (soma)
+    
+num1= int(input("Digite o primeiro numero"))
+num2= int(input("Digite o segundo numero"))
+print
